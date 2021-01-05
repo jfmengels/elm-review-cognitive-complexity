ad2ad2057d50a72691320820e64f027a4a5d400e
Jeroen Engels <jfm.engels@gmail.com>
2021-01-05 22:58:12 +0100

    Add test for duplicate calls to the same recrusive function

--- tests/CognitiveComplexityTest.elm
+++ tests/CognitiveComplexityTest.elm
@@ -174,6 +174,21 @@ fun2 n =    -- +1
                         [ ( "fun1", 1, { start = { row = 2, column = 1 }, end = { row = 2, column = 5 } } )
                         , ( "fun2", 1, { start = { row = 5, column = 1 }, end = { row = 5, column = 5 } } )
                         ]
+        , test "should increment the complexity for every recursive call in a chain, for each different function call" <|
+            \() ->
+                """module A exposing (..)
+fun1 n =
+  fun2 n    -- +1
+  + fun2 n  -- +0, already counted
+  + fun1 n  -- +1
+
+fun2 n =
+  fun1 n    -- +1
+"""
+                    |> expectComplexityAt
+                        [ ( "fun1", 2, { start = { row = 2, column = 1 }, end = { row = 2, column = 5 } } )
+                        , ( "fun2", 1, { start = { row = 7, column = 1 }, end = { row = 7, column = 5 } } )
+                        ]
         , test "the complexity of a function should not affect another function's computed complexity" <|
             \() ->
                 """module A exposing (..)
